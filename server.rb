6de7f93c094ab828d8deb7371612ce95e7b8b992
tonycarballeira <tonycarballeira@gmail.com>
2014-11-07 18:19:15 -0500

    added server project

--- server.rb
+++ server.rb
@@ -1,13 +1,15 @@
-require 'sinatra' #sinatra helps create websites/servers
+require 'sinatra' 
 
 get '/' do
-	"Hello world"
+	File.new('public/hello.txt').readlines
 end
 
 get "/sinatra" do
 	"Hello Sinatra"
 end
 
+
+#sinatra helps create websites/servers
 #'/' stands for the root directory. when someone calls the root the corresponding code block runs
 #this implements a "hello world" webpage. call the get method which takes a path ('/')
 #and a block ("hello world"). this returns the string in the block when someone issues
